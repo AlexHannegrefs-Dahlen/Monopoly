7d4c40121d0182ed55d1710b4333445e2ea95f6a
Alex <ahannegrefsdahlen@student.neumont.edu>
2016-11-11 13:53:11 -0700

    Merged Law's Game Pieces

---
+++ src/Nilrre/Jason/Alex/Law/Player.java
@@ -0,0 +1,28 @@
+package Nilrre.Jason.Alex.Law;
+
+public class Player {
+	private GamePieces piece;
+	
+	public GamePieces getPiece(GamePieces piece){
+		return piece;
+		
+	}
+	
+	public void setPiece(GamePieces newPiece){
+
+	}
+	
+	public void buyBoardPiece(){
+		
+	}
+	
+	public static void roll(){
+		
+	}
+	
+	public void buyHousesOrHotel(){
+		
+	}
+	
+	
+}
