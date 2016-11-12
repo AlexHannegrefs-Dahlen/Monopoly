d721052bef6b629dfb46f2c7219d58e1a09feef9
Alex <ahannegrefsdahlen@student.neumont.edu>
2016-11-11 21:14:53 -0700

    Added Land owning, space movement

--- src/Nilrre/Jason/Alex/Law/Player.java
+++ src/Nilrre/Jason/Alex/Law/Player.java
@@ -6,10 +6,44 @@ import java.util.Random;
 
 public class Player {
 	private String piece;
-	
-	
+	private int money;
+	private int jailCard;
+	private ArrayList<String> land;
+	private boardspace spaceCurrentlyOn;
 
-	public static String getPiece(Player pieceFinder){
+	public static void SetSpaceCurrentlyOn(Player space, boardspace spotOnBoard){
+		space.spaceCurrentlyOn = spotOnBoard;
+	}
+	
+	public static void removeLand(Player land, boardspace property) {
+		land.land.remove(property);
+	}
+
+	public static void buyLand(Player land, boardspace property) {
+		land.land.add(property);
+	}
+
+	public static int getJailcard(Player jailcard) {
+		return jailcard.jailCard;
+	}
+
+	public static void useJailCard(Player jailcard) {
+		jailcard.jailCard--;
+	}
+
+	public static void giveJailCard(Player jailcard) {
+		jailcard.jailCard++;
+	}
+
+	public static int getMoney(Player moneyFinder) {
+		return moneyFinder.money;
+	}
+
+	public static void setMoney(Player moneyVal) {
+		moneyVal.money = 1500;
+	}
+
+	public static String getPiece(Player pieceFinder) {
 		return pieceFinder.piece;
 	}
 
@@ -23,17 +57,47 @@ public class Player {
 		newPiece.piece = pieces.get(Selection - 1);
 	}
 
-	public static void buyBoardPiece() {
-
-	}
-
-	public static int roll() {
+	public static void roll(Player rolling) {
 		Random gen = new Random();
-		int movement = (gen.nextInt(11) + 2);
-		return movement;
+		int dieOne = (gen.nextInt(11) + 2);
+		int dieTwo = (gen.nextInt(11) + 2);
+		if(rolling.spaceCurrentlyOn == boardspace.inJail){
+			boolean doubles = checkForDoubles(dieOne, dieTwo);
+			if(doubles){
+				rolling.spaceCurrentlyOn = boardspace.visitingJail;
+				makeMovement(dieOne, dieTwo, rolling);
+			}
+		}
+		else{
+			int doublesCounter;
+			boolean doubles = checkForDoubles(dieOne, dieTwo);
+			if (doubles){
+				doublesCounter++;
+				if(doublesCounter == 3){
+					rolling.spaceCurrentlyOn = boardspace.inJail;
+					System.out.println("You went to jail for rolling doubles three times in a row");
+					return;
+				}
+				makeMovement(dieOne, dieTwo, rolling);
+			}
+		}
+	}
+	
+	public static void makeMovement(int dieOne, int dieTwo, Player moving){
+		int movement = dieOne + dieTwo;
+		moving.spaceCurrentlyOn = moving.spaceCurrentlyOn + movement;
 	}
 
-	public static void buyHousesOrHotel() {
+	public static boolean checkForDoubles(int dieOne, int dieTwo) {
+		boolean rolledDoubles = false;
+
+		if (dieOne == dieTwo) {
+			rolledDoubles = true;
+		}
+		return rolledDoubles;
+	}
+
+	public static void buyHousesOrHotel(boardspace property) {
 
 	}
 
