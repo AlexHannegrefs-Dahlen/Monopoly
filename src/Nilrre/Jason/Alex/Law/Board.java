d013c931fff521f0c5bb44ae78fb27bb1e2b8aeb
Errlin Deleon <EDeleon@student.neumont.edu>
2016-11-15 12:46:10 -0700

    Boards Yea Boi

---
+++ src/Nilrre/Jason/Alex/Law/Board.java
@@ -0,0 +1,57 @@
+package Nilrre.Jason.Alex;
+
+public class Board {
+
+	public BoardSpaces[][] board = new BoardSpaces[11][11];
+	BoardSpaces b = new BoardSpaces();
+	
+//	public void createBoard(BoardSpaces[][] board) {
+//		for (int i = 1; i < 10; i++) {
+//			for (int j = 1; j < 10; j++) {
+//			}
+//		}
+//	}
+
+	
+	// Print board with if statement in first for loop and make it branch out to different second for loops
+	public void printBoard(BoardSpaces[][] board) {
+
+		for(int i = 0; i < 11; i++){
+			if(board[i] < 11 && board[i] > 0){
+				
+			}
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		for(int i = 0; i < 11; i++){
+//			System.out.print(board[i][0]);
+//			for(int j = 0; j < 11; j++){
+//				if(board[i].inde < 11 && board[i][j] > 0 ){
+//					System.out.print(" ");
+//				}
+//				else{
+//					System.out.print(board[i][j]);
+//				}
+//			}
+//			System.out.print(board[i][11]);
+//			System.out.println();
+//		}
+
+	}
+	
+
+	public int Dice() {
+		int die = (int) (Math.random() * 6);
+		int die2 = (int) (Math.random() * 6);
+		return (die + die2);
+	}
+}
---
+++ src/Nilrre/Jason/Alex/Law/BoardSpaces.java
@@ -0,0 +1,81 @@
+package Nilrre.Jason.Alex;
+
+public class BoardSpaces {
+
+	private static BoardSpaces BoardSpaces;
+	private String name;
+	private String color;
+	private int rent;
+	private int morgageValue;
+	private int[] housePrice;
+	private boolean isAvailable;
+
+	public String getName() {
+		return name;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public int getRent() {
+		return rent;
+	}
+
+	public int getMorgageValue() {
+		return morgageValue;
+	}
+
+	public int[] getHousePrice() {
+		return housePrice;
+	}
+
+	public boolean getIsAvailable() {
+		return isAvailable;
+	}
+
+	public void setName(String newName) {
+		name = newName;
+	}
+
+	public void setColor(String newColor) {
+		color = newColor;
+	}
+
+	public void setRent(int newRent) {
+		rent = newRent;
+	}
+
+	public void setMorgageValue(int newMorgage) {
+		morgageValue = newMorgage;
+	}
+	
+	public void setHousePrice(int[] newHouse) {
+		housePrice = newHouse;
+	}
+	
+	public void setIsAvailable(boolean available) {
+		isAvailable = available;
+	}
+
+	public BoardSpaces getboardSpace(){
+		getColor();
+		getName();
+		getIsAvailable();
+		getRent();
+		getHousePrice();
+		getMorgageValue();
+		return BoardSpaces;
+	}
+	
+	
+	public BoardSpaces setboardSpace(String color, String Name, boolean isAvailable, int rent, int morgageValue){
+		setColor(color);
+		setName(name);
+		setIsAvailable(isAvailable);
+		setRent(rent);
+		setHousePrice(housePrice);
+		setMorgageValue(morgageValue);
+		return BoardSpaces;
+	}
+}
