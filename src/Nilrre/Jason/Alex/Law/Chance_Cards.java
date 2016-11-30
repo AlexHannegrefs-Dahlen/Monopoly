aa448eb5c4f4b619d8765a0bf446fd42fdfdd60a
Jason Esparza <jasonesparza14975@gmail.com>
2016-11-29 18:09:41 -0700

    Final?

--- src/Nilrre/Jason/Alex/Law/Chance_Cards.java
+++ src/Nilrre/Jason/Alex/Law/Chance_Cards.java
@@ -3,6 +3,9 @@ package Nilrre.Jason.Alex.Law;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
+
+import org.omg.PortableServer.ServantManagerOperations;
+
 import Nilrre.Jason.Alex.Law.Player;
 
 public class Chance_Cards {
@@ -78,8 +81,8 @@ public class Chance_Cards {
 					(Player.getSpaceCurrentlyOn(Game.getPlayerWhosTurnItIs())) - 3);
 			System.out.println("Go Back 3 Steps");
 		}
-		if (retVal == "ADV1") {//Finished?
-			Player.setSpaceCurrentlyOn(Game.getPlayerWhosTurnItIs(), 40);//EDITED
+		if (retVal == "ADV1") {//Finished
+			Player.setSpaceCurrentlyOn(Game.getPlayerWhosTurnItIs(), 40);
 			System.out.println("You advanced to Boardwalk");
 			int[] space12 = Player.spaceNumberToBoardCords(40);
 			int row40 = space12[0];
@@ -90,16 +93,21 @@ public class Chance_Cards {
 			Player.setMoney(Game.getPlayerWhosTurnItIs(), -15);
 			System.out.println("Pay the poor tax, Pay $15");
 		}
-		if (retVal == "ADV2") {//Finished?
+		if (retVal == "ADV2") {//Finished
+			int[] space12 = Player.spaceNumberToBoardCords(25);
+			int row40 = space12[0];
+			int col40 = space12[1];
 			if (Player.getSpaceCurrentlyOn(Game.getPlayerWhosTurnItIs()) == 37){
 			Player.setSpaceCurrentlyOn(Game.getPlayerWhosTurnItIs(), 25);
 			Player.setMoney(Game.getPlayerWhosTurnItIs(), 200);
 			System.out.println("You advanced to Illinois Ave., and You've Passed GO");
-			System.out.println("Collect $200");			
+			System.out.println("Collect $200");	
+			Player.checkSpaceMovedToForOwner(row40, col40 , Game.getPlayerWhosTurnItIs());
 			}
 			else{
 			Player.setSpaceCurrentlyOn(Game.getPlayerWhosTurnItIs(), 25);
 			System.out.println("You advanced to Illinois Ave.");
+			Player.checkSpaceMovedToForOwner(row40, col40 , Game.getPlayerWhosTurnItIs());
 			}
 		}
 		if (retVal == "PEP50") {//Finished
@@ -127,28 +135,64 @@ public class Chance_Cards {
 			System.out.println("You been Elected Chairman of the Board, Each player gets $50 form you");
 			System.out.println("You've Lost $" + (-50 * (Game.amountOfPlayers-1)));
 		}
-		if (retVal == "ADV2NEARRAIL") {
+		if (retVal == "ADV2NEARRAIL") {//Finished
 			if (Player.getSpaceCurrentlyOn(Game.getPlayerWhosTurnItIs()) == 8) {
+				int[] space12 = Player.spaceNumberToBoardCords(16);
+				int row12 = space12[0];
+				int col12 = space12[1];
 				Player.setSpaceCurrentlyOn(Game.getPlayerWhosTurnItIs(), 16);
 				System.out.println("You've Advanced to Pennsylvania RailRoad");
-				
-				p.PennsylvaniaRailroad();
+				if (Game.getPlayerWhosTurnItIs() != Board.b.getOwnedBy(Board.board[row12][col12]) && Board.b.getOwnedBy(Board.board[row12][col12]) != null){
+					int val = p.PennsylvaniaRailroad();
+					Player.setMoney(Game.getPlayerWhosTurnItIs(), -val);
+					Player.setMoney(Board.b.getOwnedBy(Board.board[row12][col12]), val);
+					System.out.println("But this spot is taken(and you have to pay double)");
+					Player.checkSpaceMovedToForOwner(row12, col12, Game.getPlayerWhosTurnItIs());					
+				}
+				else{
+					Player.checkSpaceMovedToForOwner(row12, col12, Game.getPlayerWhosTurnItIs());
+				}
 			}
 
 			if (Player.getSpaceCurrentlyOn(Game.getPlayerWhosTurnItIs()) == 23) {
+				int[] space12 = Player.spaceNumberToBoardCords(26);
+				int row12 = space12[0];
+				int col12 = space12[1];
 				Player.setSpaceCurrentlyOn(Game.getPlayerWhosTurnItIs(), 26);
 				System.out.println("You've Advanced to B & O Railroad");
+				if (Game.getPlayerWhosTurnItIs() != Board.b.getOwnedBy(Board.board[row12][col12]) && Board.b.getOwnedBy(Board.board[row12][col12]) != null){
+					int val = p.BandORailroad();
+					Player.setMoney(Game.getPlayerWhosTurnItIs(), -val);
+					Player.setMoney(Board.b.getOwnedBy(Board.board[row12][col12]), val);
+					System.out.println("But this spot is taken(and you have to pay double)");
+					Player.checkSpaceMovedToForOwner(row12, col12, Game.getPlayerWhosTurnItIs());
+				}
+				else{
+					Player.checkSpaceMovedToForOwner(row12, col12, Game.getPlayerWhosTurnItIs());
+				}
 			}
 			if (Player.getSpaceCurrentlyOn(Game.getPlayerWhosTurnItIs()) == 37) {
+				int[] space12 = Player.spaceNumberToBoardCords(6);
+				int row12 = space12[0];
+				int col12 = space12[1];
 				Player.setSpaceCurrentlyOn(Game.getPlayerWhosTurnItIs(), 6);
 				Player.setMoney(Game.getPlayerWhosTurnItIs(), 200);
 				System.out.println("You've Advanced to Reading Railroad, and you've passed GO");
 				System.out.println("Collect $200");
-				
+				if (Game.getPlayerWhosTurnItIs() != Board.b.getOwnedBy(Board.board[row12][col12]) && Board.b.getOwnedBy(Board.board[row12][col12]) != null){
+					int val = p.ReadingRailroad() * 2;
+					Player.setMoney(Game.getPlayerWhosTurnItIs(), -val);
+					Player.setMoney(Board.b.getOwnedBy(Board.board[row12][col12]), val);
+					System.out.println("But this spot is taken(and you have to pay double)");
+					Player.checkSpaceMovedToForOwner(row12, col12, Game.getPlayerWhosTurnItIs());
+				}
+				else{
+					Player.checkSpaceMovedToForOwner(row12, col12, Game.getPlayerWhosTurnItIs());
+				}
 			}
 
 		}
-		if (retVal == "ADV2READRAIL") {//Finished?
+		if (retVal == "ADV2READRAIL") {//Finished
 			int[] space12 = Player.spaceNumberToBoardCords(6);
 			int row12 = space12[0];
 			int col12 = space12[1];
